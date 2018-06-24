b2b2081c2c71c715653d5c4bceeeefa88ffd9a56
binjie chen <binjie09@gmail.com>
2018-06-24 14:33:47 +0800

    add and delete test

--- src/main/java/com/greglturnquist/payroll/DatabaseLoader.java
+++ src/main/java/com/greglturnquist/payroll/DatabaseLoader.java
@@ -36,6 +36,12 @@ public class DatabaseLoader implements CommandLineRunner {
 	@Override
 	public void run(String... strings) throws Exception {
 		this.repository.save(new Employee("Frodo", "Baggins", "ring bearer"));
+		this.repository.save(new Employee("Frodo", "Baggins", "ring bearer"));
+		this.repository.save(new Employee("Frodo", "Baggins", "ring bearer"));
+		this.repository.save(new Employee("Frodo", "Baggins", "ring bearer"));
+		this.repository.save(new Employee("Frodo", "Baggins", "ring bearer"));
+		this.repository.save(new Employee("Frodo", "Baggins", "ring bearer"));
+
 	}
 }
 // end::code[]
