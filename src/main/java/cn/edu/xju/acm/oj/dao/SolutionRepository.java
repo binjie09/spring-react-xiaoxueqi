a5704ed33fde2193a06cada56d5368a1c166f507
yishuida <yishuida@gamil.com>
2018-06-25 10:19:56 +0800

    add some file

--- src/main/java/cn/edu/xju/acm/oj/dao/SolutionRepository.java
+++ src/main/java/cn/edu/xju/acm/oj/dao/SolutionRepository.java
@@ -1,5 +1,6 @@
 package cn.edu.xju.acm.oj.dao;
 
+import cn.edu.xju.acm.oj.entity.Solution;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
@@ -7,5 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @date 2018/6/24 19:00
  * @Version 1.0 cn.edu.xju.acm.oj.dao
  */
-public interface Solution extends JpaRepository<Solution, Integer> {
+public interface SolutionRepository extends JpaRepository<Solution, Integer> {
 }
--- src/test/java/cn/edu/xju/acm/oj/test/ApplicationTest.java
+++ src/test/java/cn/edu/xju/acm/oj/test/ApplicationTest.java
@@ -1,13 +1,19 @@
 package cn.edu.xju.acm.oj.test;
 
 import cn.edu.xju.acm.oj.dao.CompileInfoRepository;
+import cn.edu.xju.acm.oj.dao.UserRepository;
 import cn.edu.xju.acm.oj.entity.CompileInfo;
+import cn.edu.xju.acm.oj.entity.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  * @autor yishuida
@@ -18,12 +24,28 @@ import org.springframework.test.context.junit4.SpringRunner;
 @DataJpaTest
 public class ApplicationTest {
     @Autowired
-    CompileInfoRepository repository;
+    UserRepository repository;
+
 
     @Test
-    public void contextLoads() {
-        CompileInfo info = new CompileInfo();
-        info.setError("test error");
-        repository.save(info);
+    public void addUser() {
+
+        List<User> users = new ArrayList <>();
+
+        for(int i = 0; i < 10; i ++) {
+            User user = new User();
+
+            user.setEmail(i + "@qq.com");
+            user.setAcsessTime(String.valueOf(new Date()));
+            user.setPassword("123456");
+
+            repository.save(user);
+        }
+
+        users = repository.findAll();
+
+        for (User u: users) {
+            System.out.println(u.getEmail());
+        }
     }
 }
