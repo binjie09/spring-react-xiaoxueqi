c27225d77bf16410aa335617945ea845da4cb832
binjie chen <binjie09@gmail.com>
2018-06-30 17:24:28 +0800

    make a hyperStatusList

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import CreateDialog from './components/createDialog'
+
 import HyperStatusList from './components/hyperStatusList'
 import Home from './containers/home'
 const React = require('react');
@@ -11,15 +11,11 @@ import {BrowserRouter, Route, Link} from 'react-router-dom';
 import { Menu, Icon, Form, Input, Button, Radio,Select } from 'antd';
 import RankList from "./components/rankList";
 const FormItem = Form.Item;
-
 const MenuItemGroup = Menu.ItemGroup;
 const SubMenu = Menu.SubMenu;
 const follow = require('./follow'); // function to hop multiple links by "rel"
 const root = '/api';
 
-const getConfirmation = () => {
-    // window.confirm('Are you sure?')
-}
 
 function hasErrors(fieldsError) {
     return Object.keys(fieldsError).some(field => fieldsError[field]);
@@ -157,207 +153,23 @@ class App extends React.Component {
         super(props);
         this.state = {employees: [], attributes: [], pageSize: 20, links: {}, users: []};
 
-        this.updatePageSize = this.updatePageSize.bind(this);
-        this.onCreate = this.onCreate.bind(this);
-        this.onUpdate = this.onUpdate.bind(this);
-        this.onDelete = this.onDelete.bind(this);
-        this.onNavigate = this.onNavigate.bind(this);
-        this.normFile = this.normFile.bind(this);
-    }
-    normFile (e)  {
-        console.log('Upload event:', e);
-        if (Array.isArray(e)) {
-            return e;
-        }
-        return e && e.fileList;
-    }
-    loadFromServerUser(pageSize) {
-        follow(client, root, [
-            {rel: 'users', params: {size: pageSize}}]
-        ).then(userCollection => {
-            return client({
-                method: 'GET',
-                path: userCollection.entity._links.profile.href,
-                headers: {'Accept': 'application/schema+json'}
-            }).then(schema => {
-                this.schema = schema.entity;
-                this.links = userCollection.entity._links;
-                return userCollection;
-            });
-        }).then(userCollection => {
-            return userCollection.entity._embedded.users.map(user =>
-                client({
-                    method: 'GET',
-                    path: user._links.self.href
-                })
-            );
-        }).then(userPromises => {
-            return when.all(userPromises);
-        }).done(users => {
-            this.setState({
-                users: users,
-                attributes: Object.keys(this.schema.properties),
-                pageSize: pageSize,
-                links: this.links
-            });
-        });
+
     }
 
-    // tag::follow-2[]
-    loadFromServer(pageSize) {
-        follow(client, root, [
-            {rel: 'employees', params: {size: pageSize}}]
-        ).then(employeeCollection => {
-            return client({
-                method: 'GET',
-                path: employeeCollection.entity._links.profile.href,
-                headers: {'Accept': 'application/schema+json'}
-            }).then(schema => {
-                this.schema = schema.entity;
-                this.links = employeeCollection.entity._links;
-                return employeeCollection;
-            });
-        }).then(employeeCollection => {
-            return employeeCollection.entity._embedded.employees.map(employee =>
-                client({
-                    method: 'GET',
-                    path: employee._links.self.href
-                })
-            );
-        }).then(employeePromises => {
-            return when.all(employeePromises);
-        }).done(employees => {
-            this.setState({
-                employees: employees,
-                attributes: Object.keys(this.schema.properties),
-                pageSize: pageSize,
-                links: this.links
-            });
-        });
-    }
-    // end::follow-2[]
-
-    // tag::create[]
-    onCreate(newEmployee) {
-        var self = this;
-        follow(client, root, ['employees']).then(response => {
-            return client({
-                method: 'POST',
-                path: response.entity._links.self.href,
-                entity: newEmployee,
-                headers: {'Content-Type': 'application/json'}
-            })
-        }).then(response => {
-            return follow(client, root, [{rel: 'employees', params: {'size': self.state.pageSize}}]);
-        }).done(response => {
-            if (typeof response.entity._links.last != "undefined") {
-                this.onNavigate(response.entity._links.last.href);
-            } else {
-                this.onNavigate(response.entity._links.self.href);
-            }
-        });
-    }
-    // end::create[]
-
-    // tag::update[]
-    onUpdate(employee, updatedEmployee) {
-        client({
-            method: 'PUT',
-            path: employee.entity._links.self.href,
-            entity: updatedEmployee,
-            headers: {
-                'Content-Type': 'application/json',
-                'If-Match': employee.headers.Etag
-            }
-        }).done(response => {
-            this.loadFromServer(this.state.pageSize);
-        }, response => {
-            if (response.status.code === 412) {
-                alert('DENIED: Unable to update ' +
-                    employee.entity._links.self.href + '. Your copy is stale.');
-            }
-        });
-    }
-    // end::update[]
-
-    // tag::delete[]
-    onDelete(employee) {
-        client({method: 'DELETE', path: employee.entity._links.self.href}).done(response => {
-            this.loadFromServer(this.state.pageSize);
-        });
-    }
-    // end::delete[]
-
-    // tag::navigate[]
-    onNavigate(navUri) {
-        client({
-            method: 'GET',
-            path: navUri
-        }).then(employeeCollection => {
-            this.links = employeeCollection.entity._links;
-
-            return employeeCollection.entity._embedded.employees.map(employee =>
-                client({
-                    method: 'GET',
-                    path: employee._links.self.href
-                })
-            );
-        }).then(employeePromises => {
-            return when.all(employeePromises);
-        }).done(employees => {
-            this.setState({
-                employees: employees,
-                attributes: Object.keys(this.schema.properties),
-                pageSize: this.state.pageSize,
-                links: this.links
-            });
-        });
-    }
-    // end::navigate[]
-
-    // tag::update-page-size[]
-    updatePageSize(pageSize) {
-        if (pageSize !== this.state.pageSize) {
-            this.loadFromServer(pageSize);
-        }
-    }
-    // end::update-page-size[]
-
-    // tag::follow-1[]
     componentDidMount() {
-        this.loadFromServer(this.state.pageSize);
-       // this.loadFromServerUser(this.state.pageSize);
 
     }
-    // end::follow-1[]
 
     render() {
-
-        const formItemLayout = {
-            labelCol: { span: 6 },
-            wrapperCol: { span: 14 },
-        };
         return (
-
             <div>
-
-
                 <BrowserRouter basename="/">
                     <div>
                          <Sider />
-                         <Route exact path="/ranklist" component={RankList} />
+                        <Route exact path="/ranklist" component={RankList} />
+                        <Route exact path="/" component={HyperStatusList} />
                     </div>
                 </BrowserRouter>
-
-                <CreateDialog attributes={this.state.attributes} onCreate={this.onCreate}/>
-                <HyperStatusList employees={this.state.employees}
-                              links={this.state.links}
-                              pageSize={this.state.pageSize}
-                              attributes={this.state.attributes}
-                              onNavigate={this.onNavigate}
-                              onUpdate={this.onUpdate}
-                              onDelete={this.onDelete}
-                              updatePageSize={this.updatePageSize}/>
 
             </div>
         )
--- src/main/js/components/hyperStatusList.js
+++ src/main/js/components/hyperStatusList.js
@@ -3,28 +3,157 @@
 const ReactDOM = require('react-dom')
 import React, { Component } from 'react';
 import Employee from './employee'
+import CreateDialog from './createDialog'
 const follow = require('../follow'); // function to hop multiple links by "rel"
-
 const root = '/api';
-
+const when = require('when');
+const client = require('../client')
 
 export default class HyperStatusList extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {employees: [], attributes: [], pageSize: 5, links: {}};
         this.handleNavFirst = this.handleNavFirst.bind(this);
         this.handleNavPrev = this.handleNavPrev.bind(this);
         this.handleNavNext = this.handleNavNext.bind(this);
         this.handleNavLast = this.handleNavLast.bind(this);
         this.handleInput = this.handleInput.bind(this);
+        this.updatePageSize = this.updatePageSize.bind(this);
+        this.onCreate = this.onCreate.bind(this);
+        this.onUpdate = this.onUpdate.bind(this);
+        this.onDelete = this.onDelete.bind(this);
+        this.onNavigate = this.onNavigate.bind(this);
     }
+
+    // tag::follow-2[]
+    loadFromServer(pageSize) {
+        follow(client, root, [
+            {rel: 'employees', params: {size: pageSize}}]
+        ).then(employeeCollection => {
+            return client({
+                method: 'GET',
+                path: employeeCollection.entity._links.profile.href,
+                headers: {'Accept': 'application/schema+json'}
+            }).then(schema => {
+                this.schema = schema.entity;
+                this.links = employeeCollection.entity._links;
+                return employeeCollection;
+            });
+        }).then(employeeCollection => {
+            return employeeCollection.entity._embedded.employees.map(employee =>
+                client({
+                    method: 'GET',
+                    path: employee._links.self.href
+                })
+            );
+        }).then(employeePromises => {
+            return when.all(employeePromises);
+        }).done(employees => {
+            this.setState({
+                employees: employees,
+                attributes: Object.keys(this.schema.properties),
+                pageSize: pageSize,
+                links: this.links
+            });
+        });
+    }
+    // end::follow-2[]
+
+    // tag::create[]
+    onCreate(newEmployee) {
+        var self = this;
+        follow(client, root, ['employees']).then(response => {
+            return client({
+                method: 'POST',
+                path: response.entity._links.self.href,
+                entity: newEmployee,
+                headers: {'Content-Type': 'application/json'}
+            })
+        }).then(response => {
+            return follow(client, root, [{rel: 'employees', params: {'size': self.state.pageSize}}]);
+        }).done(response => {
+            if (typeof response.entity._links.last != "undefined") {
+                this.onNavigate(response.entity._links.last.href);
+            } else {
+                this.onNavigate(response.entity._links.self.href);
+            }
+        });
+    }
+    // end::create[]
+
+    // tag::update[]
+    onUpdate(employee, updatedEmployee) {
+        client({
+            method: 'PUT',
+            path: employee.entity._links.self.href,
+            entity: updatedEmployee,
+            headers: {
+                'Content-Type': 'application/json',
+                'If-Match': employee.headers.Etag
+            }
+        }).done(response => {
+            this.loadFromServer(this.state.pageSize);
+        }, response => {
+            if (response.status.code === 412) {
+                alert('DENIED: Unable to update ' +
+                    employee.entity._links.self.href + '. Your copy is stale.');
+            }
+        });
+    }
+    // end::update[]
+
+    // tag::delete[]
+    onDelete(employee) {
+        client({method: 'DELETE', path: employee.entity._links.self.href}).done(response => {
+            this.loadFromServer(this.state.pageSize);
+        });
+    }
+    // end::delete[]
+
+    // tag::navigate[]
+    onNavigate(navUri) {
+        client({
+            method: 'GET',
+            path: navUri
+        }).then(employeeCollection => {
+            this.links = employeeCollection.entity._links;
+
+            return employeeCollection.entity._embedded.employees.map(employee =>
+                client({
+                    method: 'GET',
+                    path: employee._links.self.href
+                })
+            );
+        }).then(employeePromises => {
+            return when.all(employeePromises);
+        }).done(employees => {
+            this.setState({
+                employees: employees,
+                attributes: Object.keys(this.schema.properties),
+                pageSize: this.state.pageSize,
+                links: this.links
+            });
+        });
+    }
+    // end::navigate[]
+
+    // tag::update-page-size[]
+    updatePageSize(pageSize) {
+        if (pageSize !== this.state.pageSize) {
+            this.loadFromServer(pageSize);
+        }
+    }
+    // end::update-page-size[]
+
+
 
     // tag::handle-page-size-updates[]
     handleInput(e) {
         e.preventDefault();
         var pageSize = ReactDOM.findDOMNode(this.refs.pageSize).value;
         if (/^[0-9]+$/.test(pageSize)) {
-            this.props.updatePageSize(pageSize);
+            this.updatePageSize(pageSize);
         } else {
             ReactDOM.findDOMNode(this.refs.pageSize).value = pageSize.substring(0, pageSize.length - 1);
         }
@@ -34,48 +163,58 @@ export default class HyperStatusList extends Component {
     // tag::handle-nav[]
     handleNavFirst(e){
         e.preventDefault();
-        this.props.onNavigate(this.props.links.first.href);
+        this.onNavigate(this.state.links.first.href);
     }
     handleNavPrev(e) {
         e.preventDefault();
-        this.props.onNavigate(this.props.links.prev.href);
+        this.onNavigate(this.state.links.prev.href);
     }
     handleNavNext(e) {
         e.preventDefault();
-        this.props.onNavigate(this.props.links.next.href);
+        this.onNavigate(this.state.links.next.href);
     }
     handleNavLast(e) {
         e.preventDefault();
-        this.props.onNavigate(this.props.links.last.href);
+        this.onNavigate(this.state.links.last.href);
     }
     // end::handle-nav[]
     // tag::employee-list-render[]
+    // tag::follow-1[]
+    componentDidMount() {
+        this.loadFromServer(this.state.pageSize);
+        // this.loadFromServerUser(this.state.pageSize);
+
+    }
+    // end::follow-1[]
     render() {
-        var employees = this.props.employees.map(employee =>
+        var employees = this.state.employees.map(employee =>
             <Employee key={employee.entity._links.self.href}
                       employee={employee}
-                      attributes={this.props.attributes}
-                      onUpdate={this.props.onUpdate}
-                      onDelete={this.props.onDelete}/>
+                      attributes={this.state.attributes}
+                      onUpdate={this.onUpdate}
+                      onDelete={this.onDelete}/>
         );
 
         var navLinks = [];
-        if ("first" in this.props.links) {
-            navLinks.push(<button key="first" onClick={this.handleNavFirst}>&lt;&lt;</button>);
+
+        if ("first" in this.state.links) {
+            navLinks.push(<button key="first" onClick={this.handleNavFirst}>第一页</button>);
         }
-        if ("prev" in this.props.links) {
-            navLinks.push(<button key="prev" onClick={this.handleNavPrev}>&lt;</button>);
+        if ("prev" in this.state.links) {
+            navLinks.push(<button key="prev" onClick={this.handleNavPrev}>上一页</button>);
+           // curpages = this.state.links.prev ;
         }
-        if ("next" in this.props.links) {
-            navLinks.push(<button key="next" onClick={this.handleNavNext}>&gt;</button>);
+        if ("next" in this.state.links) {
+            navLinks.push(<button key="next" onClick={this.handleNavNext}>下一页</button>);
+
         }
-        if ("last" in this.props.links) {
-            navLinks.push(<button key="last" onClick={this.handleNavLast}>&gt;&gt;</button>);
+        if ("last" in this.state.links) {
+            navLinks.push(<button key="last" onClick={this.handleNavLast}>最后一页</button>);
         }
 
         return (
             <div>
-                <input ref="pageSize" defaultValue={this.props.pageSize} onInput={this.handleInput}/>
+                <input ref="pageSize" defaultValue={this.state.pageSize} onInput={this.handleInput}/>
                 <table>
                     <tbody>
                     <tr>
@@ -90,7 +229,10 @@ export default class HyperStatusList extends Component {
                 </table>
                 <div>
                     {navLinks}
+
+
                 </div>
+                <CreateDialog attributes={this.state.attributes} onCreate={this.onCreate}/>
             </div>
         )
     }
